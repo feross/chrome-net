a325816cdac250272c9875a771de2c71d98d67cf
Feross Aboukhadijeh <feross@feross.org>
2014-07-23 18:22:25 -0700

    Fix bug in Socket.prototype.setTimeout (#8)

--- index.js
+++ index.js
@@ -699,7 +699,7 @@ Socket.prototype._resetTimeout = function () {
     clearTimeout(self._timeout)
   }
   if (self._timeoutMs) {
-    self._timeout = setTimeout(self._onTimeout.bind(self), self.timeoutMs)
+    self._timeout = setTimeout(self._onTimeout.bind(self), self._timeoutMs)
   }
 }
 
