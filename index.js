530442e0447d0d26d4888f21901ee3cfe847c313
Feross Aboukhadijeh <feross@feross.org>
2014-09-30 11:06:01 -0700

    style

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function onReceiveError (info) {
   if (info.socketId in sockets) {
     sockets[info.socketId]._onReceiveError(info.resultCode)
   } else {
-    if (info.resultCode == -100) // net::ERR_CONNECTION_CLOSED
+    if (info.resultCode === -100) // net::ERR_CONNECTION_CLOSED
       return
     console.error('Unknown socket id: ' + info.socketId)
   }
